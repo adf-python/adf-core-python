92ef7a76748e24c8421b3746c2dedf9ba4c7f0de
shima004 <yellow030408@gmail.com>
2024-09-03 18:42:27 +0900

    feat: Add AStarPathPlanning module for path planning

---
+++ adf_core_python/implement/module/astar_path_planning.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List
+
+from adf_core_python.core.component.module.algorithm.path_planning import (
+    PathPlanning,
+)
+
+if TYPE_CHECKING:
+    from rcrs_core.worldmodel.entityID import EntityID
+
+
+class AStarPathPlanning(PathPlanning):
+    def get_path(
+        self, from_entity_id: EntityID, to_entity_id: EntityID
+    ) -> List[EntityID]:
+        return []
+
+    def get_distance(self, from_entity_id: EntityID, to_entity_id: EntityID) -> float:
+        return 0.0
+
+    def calculate(self) -> AStarPathPlanning:
+        return self
