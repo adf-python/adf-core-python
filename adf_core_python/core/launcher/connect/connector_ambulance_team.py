e3f3cd4e78eef961ea2836500bbd23494fee8b09
shima004 <yellow030408@gmail.com>
2024-09-18 15:03:27 +0900

    feat: Update connector classes to use Config from adf_core_python.core.config.config

--- adf_core_python/core/launcher/connect/connector_ambulance_team.py
+++ adf_core_python/core/launcher/connect/connector_ambulance_team.py
@@ -1,12 +1,15 @@
 from logging import Logger, getLogger
 
 from rcrs_core.agents.ambulanceTeamAgent import AmbulanceTeamAgent
-from rcrs_core.config.config import Config
 from rcrs_core.connection.componentLauncher import ComponentLauncher
 
 from adf_core_python.core.agent.config.module_config import ModuleConfig
 from adf_core_python.core.agent.develop.develop_data import DevelopData
 from adf_core_python.core.component.abstract_loader import AbstractLoader
+from adf_core_python.core.component.tactics.tactics_ambulance_team import (
+    TacticsAmbulanceTeam,
+)
+from adf_core_python.core.config.config import Config
 from adf_core_python.core.launcher.config_key import ConfigKey
 from adf_core_python.core.launcher.connect.connector import Connector
 
@@ -22,31 +25,28 @@ class ConnectorAmbulanceTeam(Connector):
         config: Config,
         loader: AbstractLoader,
     ) -> None:
-        count: int = config.get_int_value_or_default(
-            ConfigKey.KEY_AMBULANCE_CENTRE_COUNT, 0
-        )
+        count: int = config.get_value(ConfigKey.KEY_AMBULANCE_CENTRE_COUNT, 0)
         if count == 0:
             return
 
         for _ in range(count):
-            # tactics_ambulance_team: TacticsAmbulanceTeam
-            if loader.get_tactics_ambulance_team() is not None:
+            if loader.get_tactics_ambulance_team() is None:
                 self.logger.error("Cannot load ambulance team tactics")
-                # tactics_ambulance_team = loader.get_tactics_ambulance_team()
-            else:
-                # tactics_ambulance_team = DummyTacticsAmbulanceTeam()
-                pass
+
+            tactics_ambulance_team: TacticsAmbulanceTeam = (
+                loader.get_tactics_ambulance_team()
+            )
 
             module_config: ModuleConfig = ModuleConfig(  # noqa: F841
-                config.get_value_or_default(
+                config.get_value(
                     ConfigKey.KEY_MODULE_CONFIG_FILE_NAME,
                     ModuleConfig.DEFAULT_CONFIG_FILE_NAME,
                 )
             )
 
             develop_data: DevelopData = DevelopData(  # noqa: F841
-                config.get_boolean_value_or_default(ConfigKey.KEY_DEBUG_FLAG, False),
-                config.get_value_or_default(
+                config.get_value(ConfigKey.KEY_DEBUG_FLAG, False),
+                config.get_value(
                     ConfigKey.KEY_DEVELOP_DATA_FILE_NAME, DevelopData.DEFAULT_FILE_NAME
                 ),
             )
@@ -54,9 +54,7 @@ class ConnectorAmbulanceTeam(Connector):
             # TODO: component_launcher.generate_request_ID can cause race condition
             component_launcher.connect(
                 AmbulanceTeamAgent(
-                    config.get_boolean_value_or_default(
-                        ConfigKey.KEY_PRECOMPUTE, False
-                    ),
+                    config.get_value(ConfigKey.KEY_PRECOMPUTE, False),
                 ),
                 component_launcher.generate_request_ID(),
             )
--- adf_core_python/core/launcher/connect/connector_fire_brigade.py
+++ adf_core_python/core/launcher/connect/connector_fire_brigade.py
@@ -1,12 +1,12 @@
 from logging import Logger, getLogger
 
 from rcrs_core.agents.fireBrigadeAgent import FireBrigadeAgent
-from rcrs_core.config.config import Config
 from rcrs_core.connection.componentLauncher import ComponentLauncher
 
 from adf_core_python.core.agent.config.module_config import ModuleConfig
 from adf_core_python.core.agent.develop.develop_data import DevelopData
 from adf_core_python.core.component.abstract_loader import AbstractLoader
+from adf_core_python.core.config.config import Config
 from adf_core_python.core.launcher.config_key import ConfigKey
 from adf_core_python.core.launcher.connect.connector import Connector
 
@@ -22,9 +22,7 @@ class ConnectorFireBrigade(Connector):
         config: Config,
         loader: AbstractLoader,
     ) -> None:
-        count: int = config.get_int_value_or_default(
-            ConfigKey.KEY_AMBULANCE_CENTRE_COUNT, 0
-        )
+        count: int = config.get_value(ConfigKey.KEY_AMBULANCE_CENTRE_COUNT, 0)
         if count == 0:
             return
 
@@ -38,15 +36,15 @@ class ConnectorFireBrigade(Connector):
                 pass
 
             module_config: ModuleConfig = ModuleConfig(  # noqa: F841
-                config.get_value_or_default(
+                config.get_value(
                     ConfigKey.KEY_MODULE_CONFIG_FILE_NAME,
                     ModuleConfig.DEFAULT_CONFIG_FILE_NAME,
                 )
             )
 
             develop_data: DevelopData = DevelopData(  # noqa: F841
-                config.get_boolean_value_or_default(ConfigKey.KEY_DEBUG_FLAG, False),
-                config.get_value_or_default(
+                config.get_value(ConfigKey.KEY_DEBUG_FLAG, False),
+                config.get_value(
                     ConfigKey.KEY_DEVELOP_DATA_FILE_NAME, DevelopData.DEFAULT_FILE_NAME
                 ),
             )
@@ -54,9 +52,7 @@ class ConnectorFireBrigade(Connector):
             # TODO: component_launcher.generate_request_ID can cause race condition
             component_launcher.connect(
                 FireBrigadeAgent(
-                    config.get_boolean_value_or_default(
-                        ConfigKey.KEY_PRECOMPUTE, False
-                    ),
+                    config.get_value(ConfigKey.KEY_PRECOMPUTE, False),
                 ),
                 component_launcher.generate_request_ID(),
             )
