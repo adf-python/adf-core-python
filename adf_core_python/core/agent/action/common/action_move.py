1ac267542bfe4f5d5b2eba82b5b881afb53a04c3
shima004 <yellow030408@gmail.com>
2024-09-25 10:44:22 +0900

    feat: add lint N8 check

--- adf_core_python/core/agent/action/common/action_move.py
+++ adf_core_python/core/agent/action/common/action_move.py
@@ -15,19 +15,19 @@ class ActionMove(Action):
     def __init__(
         self,
         path: list[EntityID],
-        destinationX: Optional[int] = None,
-        destinationY: Optional[int] = None,
+        destination_x: Optional[int] = None,
+        destination_y: Optional[int] = None,
     ) -> None:
         self.path = path
-        self.destinationX = destinationX
-        self.destinationY = destinationY
+        self.destination_x = destination_x
+        self.destination_y = destination_y
 
     def get_command(self, agent_id: EntityID, time: int) -> Command:
         path: list[int] = [p.get_value() for p in self.path]
-        if self.destinationX is not None and self.destinationY is not None:
-            return AKMove(agent_id, time, path, self.destinationX, self.destinationY)
+        if self.destination_x is not None and self.destination_y is not None:
+            return AKMove(agent_id, time, path, self.destination_x, self.destination_y)
         else:
             return AKMove(agent_id, time, path)
 
     def __str__(self) -> str:
-        return f"ActionMove(path={self.path}, destinationX={self.destinationX}, destinationY={self.destinationY})"
+        return f"ActionMove(path={self.path}, destination_x={self.destination_x}, destination_y{self.destination_y})"
