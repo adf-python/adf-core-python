4157c4eabca38e7202b8516bc43774a53dbe951a
harrki <spemting@gmail.com>
2024-10-03 20:07:42 +0900

    fix: create new directory and move astar path planning module

--- adf_core_python/implement/module/algorithm/astar_path_planning.py
+++ adf_core_python/implement/module/algorithm/astar_path_planning.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from rcrs_core.worldmodel.entityID import EntityID
+
 from adf_core_python.core.component.module.algorithm.path_planning import (
     PathPlanning,
 )
@@ -11,6 +13,9 @@ if TYPE_CHECKING:
 
 
 class AStarPathPlanning(PathPlanning):
+    def get_result(self) -> list[EntityID]:
+        return self._result
+
     def get_path(
         self, from_entity_id: EntityID, to_entity_id: EntityID
     ) -> list[EntityID]:
@@ -19,5 +24,17 @@ class AStarPathPlanning(PathPlanning):
     def get_distance(self, from_entity_id: EntityID, to_entity_id: EntityID) -> float:
         return 0.0
 
+    def set_from(self, from_entity_id: EntityID) -> PathPlanning:
+        self._from = from_entity_id
+        return self
+
+    def set_destination(self, destination_entity_ids: list[EntityID]) -> PathPlanning:
+        self._targets = destination_entity_ids
+        return self
+
     def calculate(self) -> AStarPathPlanning:
         return self
+
+
+# TODO: Implement the A* path planning algorithm
+# TODO: Move this file to algorithm directory
