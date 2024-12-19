98c1db930d08e44048a6b1b28829177b55ca7095
shima004 <yellow030408@gmail.com>
2024-12-19 16:59:12 +0900

    feat: Add versioning support and improve documentation localization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import yaml
 
 sys.path.insert(0, os.path.abspath("../../"))
 
@@ -32,10 +33,55 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = []
 
-language = "ja"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+locale_dirs = ["locale"]
+language = "en"
+
+# get the environment variable build_all_docs and pages_root
+build_all_docs = os.environ.get("build_all_docs")
+pages_root = os.environ.get("pages_root", "")
+
+# if not there, we dont call this
+if build_all_docs is not None:
+    # we get the current language and version
+    current_language = os.environ.get("current_language")
+    current_version = os.environ.get("current_version")
+
+    # we set the html_context wit current language and version 
+    # and empty languages and versions for now
+    html_context = {
+        'current_language' : current_language,
+        'languages' : [],
+        'current_version' : current_version,
+        'versions' : [],
+    }
+
+
+    # and we append all versions and langauges accordingly 
+    # we treat t he main branch as latest 
+    if (current_version == 'latest'):
+        html_context['languages'].append(['en', pages_root])
+        html_context['languages'].append(['ja', pages_root+'/ja'])
+
+    if (current_language == 'en'):
+        html_context['versions'].append(['latest', pages_root])
+    if (current_language == 'ja'):
+        html_context['versions'].append(['latest', pages_root+'/ja'])
+
+    # and loop over all other versions from our yaml file
+    # to set versions and languages
+    with open("../versions.yaml", "r") as yaml_file:
+        docs = yaml.safe_load(yaml_file)
+
+    if (current_version != 'latest'):
+        for language in docs[current_version].get('languages', []):
+            html_context['languages'].append([language, pages_root+'/'+current_version+'/'+language])
+
+    for version, details in docs.items():
+        html_context['versions'].append([version, pages_root+'/'+version+'/'+current_language])
